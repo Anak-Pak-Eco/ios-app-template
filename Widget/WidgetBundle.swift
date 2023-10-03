5a564967e7331ade051581e691576ee1737ff8d1
Leonardo472 <leonardojose040702@gmail.com>
2023-10-03 13:52:28 +0700

    Create widget to project

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Leonardo Jose Gunawan on 02/10/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+//@main
+//struct WidgetBundle: WidgetBundle {
+//    var body: some Widget {
+//        Widget()
+//    }
+//}
